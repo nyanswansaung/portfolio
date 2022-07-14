3d09c8706bf3be78353c5501c8b259584c7b13e3
NyanSwanSaung <nyanswansaung.dev@protonmail.com>
2022-07-14 16:08:11 +0630

    Early stage of responsive

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 let menu = document.querySelector('.menu-icon')
+let navbar = document.querySelector('.navbar')
 
 menu.onclick = () => {
     menu.classList.toggle('move');
+    navbar.classList.toggle('open-menu')
+}
+
+window.onscroll= () => {
+    navbar.classList.remove('open-menu')
+    menu.classList.remove('move')
 }
